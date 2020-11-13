40ef303f5920aa80614e00620ec249c1bc4e4904
Anwar Hussen <anwar.hussen4@gmail.com>
2020-11-13 14:11:00 +0600

    code viewed

--- resources/views/define-callback.blade.php
+++ resources/views/define-callback.blade.php
@@ -2,18 +2,26 @@
 
 @section('content')
     <section id="second-buyer-elo">
-
+        <code>
+            <pre>
+                &lt;script&gt;
+                    function checkAge(data, callback) {
+                        callback(data);
+                    }
+                &lt;/script&gt;
+            </pre>
+        </code>
     </section>
 @endsection
 
 @push('js')
     <script>
-
         function checkAge(data, callback) {
             callback(data);
         }
-
-        var data = {email: 'trump@gmail.com', age: 17}
+    </script>
+    <script>
+        var data = {email: 'trump@gmail.com', age: 79}
         checkAge(data, function (email) {
             console.log(email.age < 18 ? 'Email is not valid' : 'Email is valid') //If data.age < 18 it'll log as not valid
         })
